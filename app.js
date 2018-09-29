3d7485b65171b452f286bd8bacfd838650baae05
TurtleWolfe <jonpohlner@gmail.com>
2018-09-29 05:57:30 -0400

    added keyset from tutorial & noticed TerraPheonix, miss you babe

--- app.js
+++ app.js
@@ -1,43 +1,62 @@
-// const form = document.querySelector("form");
-const taskInput = document.getElementById("task");
-const heading = document.querySelector("h2");
-// const select = document.querySelector("select");
-const body = document.querySelector("body");
-// Clear input
-// taskInput.value = "";
+// // const form = document.querySelector("form");
+// const taskInput = document.getElementById("task");
+// const heading = document.querySelector("h2");
+// // const select = document.querySelector("select");
+// const body = document.querySelector("body");
+// // Clear input
+// // taskInput.value = "";
 
-// Keypress
-//taskInput.addEventListener("keypress", runEvent);
-body.addEventListener("keypress", runEvent);
-// Focus
-// taskInput.addEventListener('focus', runEvent);
-// Blur
-// taskInput.addEventListener('blur', runEvent);
-// Input
-// taskInput.addEventListener('input', runEvent);
-// Change
-// select.addEventListener('change', runEvent);
+// // Keypress
+// //taskInput.addEventListener("keypress", runEvent);
+// body.addEventListener("keypress", runEvent);
+// // Focus
+// // taskInput.addEventListener('focus', runEvent);
+// // Blur
+// // taskInput.addEventListener('blur', runEvent);
+// // Input
+// // taskInput.addEventListener('input', runEvent);
+// // Change
+// // select.addEventListener('change', runEvent);
 
-let globalVolume = 1;
-window.SetVolume = function(val) {
-  var player = document.getElementsByTagName("audio");
-  console.log("Before: " + player.volume);
-  player.volume = val / 100;
-  globalVolume = player.volume;
-  console.log("After: " + player.volume);
-};
-function runEvent(e) {
-  const keyName = event.key;
-  //audio.volume = globalVolume; // trying to apply this audio volume to note below
-  const note = document.querySelector(`[data-sound-id="${keyName}"]`).play();
+// let globalVolume = 1;
+// window.SetVolume = function(val) {
+//   var player = document.getElementsByTagName("audio");
+//   console.log("Before: " + player.volume);
+//   player.volume = val / 100;
+//   globalVolume = player.volume;
+//   console.log("After: " + player.volume);
+// };
+// function runEvent(e) {
+//   const keyName = event.key;
+//   //audio.volume = globalVolume; // trying to apply this audio volume to note below
+//   const note = document.querySelector(`[data-sound-id="${keyName}"]`).play();
 
-  // alert("keypress event\n\n" + "key: " + keyName);
-  console.log(`EVENT TYPE: ${e.type}`);
-  console.log(e.target.value);
-  console.log(keyName);
-  heading.value = "";
-  heading.innerText = keyName;
-  // Get input value
-  // console.log(taskInput.value);
-  // e.preventDefault();
+//   // alert("keypress event\n\n" + "key: " + keyName);
+//   console.log(`EVENT TYPE: ${e.type}`);
+//   console.log(e.target.value);
+//   console.log(keyName);
+//   heading.value = "";
+//   heading.innerText = keyName;
+//   // Get input value
+//   // console.log(taskInput.value);
+//   // e.preventDefault();
+// }
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
 }
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
